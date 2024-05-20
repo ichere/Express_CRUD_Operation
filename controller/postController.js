7cb0cd64acf7303051a79761565949953e110baa
unknown <izuagbalachere@gmail.com>
2024-05-20 22:17:11 +0100

    created the controllers

--- controller/postController.js
+++ controller/postController.js
@@ -1,6 +1,15 @@
+let posts = [
+    {id: 1, title: "my first post"},
+    {id: 2, title: "my second post"},
+    {id: 3, title: "my third post"},
+    {id: 4, title: "my fourth post"},
+    {id: 5, title: "my fifth post"},
+    {id: 6, title: "my sixth post"},
+];
+
 
 // Get all Posts route: /api/posts
-const getPosts = (req, res, next) => {
+export const getPosts = (req, res, next) => {
     const limit = parseInt(req.query.limit);
 
     if(!isNaN(limit) && limit > 0){
@@ -11,8 +20,8 @@ const getPosts = (req, res, next) => {
     
 }
 
-//Get single Post route: /api/posts/:id
-const getPost = (req, res, next) => {
+//Get single Post GET route: /api/posts/:id
+export const getPost = (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id );
 
@@ -21,4 +30,54 @@ const getPost = (req, res, next) => {
         error.status = 404;
         return next(error);
     } 
+}
+
+//Create a post  POST route: /api/posts/
+export const createPost = (req, res, next) => {
+    const newPost = {
+        id: posts.length + 1,
+        title: req.body.title
+    }
+
+    if(!newPost.title){
+        // return res.status(400).json({msg: 'Please include a title'});
+        const error = new Error(`Please include a title!`);
+        error.status = 400;
+        return next(error);
+    }
+
+    posts.push(newPost);
+    res.status(201).json(posts);
+}
+
+//Update a post  PUT route: /api/posts/:id
+export const updatePost = (req, res, next) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        // return res.status(404).json({ msg: `No post for the id ${id}`});
+        const error = new Error(`No post for the ${id} `);
+        error.status = 404;
+        return next(error);
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+}
+
+//Delete a post  DELETE route: /api/posts/:id
+export const deletePost = (req, res, next) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        // return res.status(404).json({ msg: `No post for the id ${id}`});
+        const error = new Error(`No post for the ${id} `);
+        error.status = 404;
+        return next(error);
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,6 @@
 import express from 'express';
 const router = express.Router();
-
-let posts = [
-    {id: 1, title: "my first post"},
-    {id: 2, title: "my second post"},
-    {id: 3, title: "my third post"},
-    {id: 4, title: "my fourth post"},
-    {id: 5, title: "my fifth post"},
-    {id: 6, title: "my sixth post"},
-];
+import { createPost, deletePost, getPost, getPosts, updatePost } from '../controller/postController.js';
 
 
 // static setup
@@ -20,7 +12,7 @@ let posts = [
 // });
 
 //Get all posts with limit
-router.get('/', );
+router.get('/', getPosts);
 
 //Get all posts
 router.get('/', (req, res, next) => {
@@ -40,7 +32,7 @@ router.get('/', (req, res, next) => {
 // });
 
 // New Error handling for single post
-router.get('/:id', );
+router.get('/:id', getPost);
 
 
 //Get one posts
@@ -50,54 +42,13 @@ router.get('/:id', (req, res, next) => {
 });
 
 //Create new post
-router.post('/', (req, res, next) => {
-    const newPost = {
-        id: posts.length + 1,
-        title: req.body.title
-    }
-
-    if(!newPost.title){
-        // return res.status(400).json({msg: 'Please include a title'});
-        const error = new Error(`Please include a title!`);
-        error.status = 400;
-        return next(error);
-    }
-
-    posts.push(newPost);
-    res.status(201).json(posts);
-});
+router.post('/', createPost);
 
 //Update Post
-router.put('/:id', (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if(!post){
-        // return res.status(404).json({ msg: `No post for the id ${id}`});
-        const error = new Error(`No post for the ${id} `);
-        error.status = 404;
-        return next(error);
-    }
-
-    post.title = req.body.title;
-    res.status(200).json(posts);
-});
+router.put('/:id', updatePost);
 
 //Delete Post
-router.delete('/:id', (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if(!post){
-        // return res.status(404).json({ msg: `No post for the id ${id}`});
-        const error = new Error(`No post for the ${id} `);
-        error.status = 404;
-        return next(error);
-    }
-
-    posts = posts.filter((post) => post.id !== id);
-    res.status(200).json(posts);
-});
+router.delete('/:id', deletePost);
 
 // module.exports = router; for common.js
 export default router;
